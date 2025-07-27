c93d240d6c451637b4c13a32175bab4040eec9df
ManamiShimizu081 <orange2mana15@icloud.com>
2025-07-27 18:39:03 +0900

    ページ切り替え

--- docs/script.js
+++ docs/script.js
@@ -46,3 +46,17 @@ for (let i = 0; i < dropCount; i++) {
    clone.classList.add("mirror");
 
    document.getElementById("mirror-container").appendChild(clone);
+
+
+//画面切り替え
+    window.addEventListener('scroll', function () {
+      const scrollTop = window.scrollY;
+      const windowHeight = window.innerHeight;
+      const docHeight = document.body.scrollHeight;
+
+      if (scrollTop + windowHeight >= docHeight) {
+        // 最下部に到達したら next.html に遷移
+        window.location.href = "next.html";
+      }
+      
+    });
