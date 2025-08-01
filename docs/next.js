c8aec485ff1e7675a2817ecd53c4787028afe413
ManamiShimizu081 <orange2mana15@icloud.com>
2025-08-02 00:23:51 +0900

    前半

--- docs/next.js
+++ docs/next.js
@@ -1,42 +1,4 @@
-//雨を降らせるーーーーーーーーーーーーーー
 
-const dropCount = 10;
-const body = document.body;
-
-// ページの高さを取得
-const pageHeight = Math.max(
-  document.body.scrollHeight,
-  document.documentElement.scrollHeight
-);
-
-// @keyframes を動的に定義
-const styleSheet = document.createElement("style");
-styleSheet.innerHTML = `
-  @keyframes fallDynamic {
-    0% { top: -20px;  }
-    100% { top: 2700px;  }
-  }
-`;
-document.head.appendChild(styleSheet);
-
-// 雨粒を生成
-for (let i = 0; i < dropCount; i++) {
-  const drop = document.createElement('div');
-  drop.classList.add('drop');
-
-  const x = Math.random() * window.innerWidth;
-  const duration = 5 + Math.random() * 20;
-  const delay = Math.random() * 5;
-  const size = 15;
-
-  drop.style.left = `${x}px`;
-  drop.style.width = `${size}%`;
-  drop.style.height = `${size}%`;
-  drop.style.animation = `fallDynamic ${duration}s linear infinite`;
-  drop.style.animationDelay = `${delay}s`;
-
-  body.appendChild(drop);
-}
 
 
 //鏡面反射-----------------------------------
